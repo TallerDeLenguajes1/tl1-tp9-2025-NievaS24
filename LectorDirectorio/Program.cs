6869bff558c29f5eca21af08cd61980cf2d72980
NievaS24 <santinieva24@gmail.com>
2025-05-18 14:18:59 -0300

    Escribir datos de los archivos en un .csv.

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -16,12 +16,20 @@ foreach (string directorio in directorios)
     Console.WriteLine($"\t {directorioNombre[longitud - 1]}"); //Lista el ultimo elemento del arreglo, en este caso es el nombre del directorio
 }
 string [] archivos = Directory.GetFiles(direccion);
-Console.WriteLine($"Los archivos dentro de {direccion} son:");
-foreach (string archivo in archivos)
+string rutaArchivoCsv = string.Concat(direccion,"\\reporte_archivos.csv");
+using (StreamWriter sw = new StreamWriter(rutaArchivoCsv))
 {
-    FileInfo miArchivo = new FileInfo(archivo);
-    string [] archivoNombre = archivo.Split("\\");
-    int longitud = archivoNombre.Length;
-    double tamaño = miArchivo.Length / 1024.00; //.Length da el tamaño en bytes
-    Console.WriteLine($"\t {archivoNombre[longitud - 1]} - Tamaño: {tamaño:0.00} KB");
+    Console.WriteLine($"Los archivos dentro de {direccion} son:");
+    sw.WriteLine("Nombre - Tamanio - Ultima Modificacion");
+    foreach (string archivo in archivos)
+    {
+        FileInfo miArchivo = new FileInfo(archivo);
+        string [] archivoNombre = archivo.Split("\\");
+        int longitud = archivoNombre.Length;
+        double tamaño = miArchivo.Length / 1024.00; //.Length da el tamaño en bytes
+        DateTime fechaMod = miArchivo.LastWriteTime;
+        Console.WriteLine($"\t {archivoNombre[longitud - 1]} - Tamaño: {tamaño:0.00} KB");
+        sw.WriteLine($"{archivoNombre[longitud - 1]} - {tamaño:0.00} Kb - {fechaMod:dd/MM/yyyy HH:mm}");
+    }
+    
 }
