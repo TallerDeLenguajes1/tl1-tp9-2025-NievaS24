852af1c790a4999894b1f97394da406d7dc78a77
NievaS24 <santinieva24@gmail.com>
2025-05-18 12:32:43 -0300

    Ingreso y control de un path valido.

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string direccion;
+Console.WriteLine("Ingrese un path de un directorio:");
+direccion = Console.ReadLine();
+bool existe = Directory.Exists(direccion);
+while (!existe)
+{
+    Console.WriteLine("El directorio ingresado no existe. Ingrese uno valido por favor:");
+    direccion = Console.ReadLine();
+    existe = Directory.Exists(direccion);
+}
